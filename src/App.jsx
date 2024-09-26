a0e6e3750038c98ada9549a45836f697ef34a66c
Axel Gumiit <149522466+AxelGumiit@users.noreply.github.com>
2024-09-26 15:03:44 +0100

    added a working character

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [19, 19, 19], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
   return (
     <>
+    <Environment preset="sunset"></Environment>
+    <ambientLight intensity={0.5}></ambientLight>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Avatar/>
     </>
   );
 };
