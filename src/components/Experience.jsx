f82edc193f96348f9d5a2f41b1e15972298a99ba
Axel <axelgumiit2@gmail.com>
2024-10-15 13:51:20 +0100

    add interactive desk

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,15 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Avatar2 } from "./Avatar2";
+import { useRef, useState } from "react";
+import { Table } from "./desk";
 
 export const Experience = () => {
+  const myMesh = useRef();
+  const [active, setActive] = useState(false)
   return (
+
     <>
+    
     <Environment preset="sunset"></Environment>
     <ambientLight intensity={0.5}></ambientLight>
     
@@ -12,6 +18,12 @@ export const Experience = () => {
       <planeGeometry args ={[10,10]}/>
       <meshStandardMaterial color="lightpurple"/>
       </mesh>
+
+      <group position-z={4}>
+      <mesh scale = {active ? 2 : 1} onClick={() => setActive(!active)} ref ={myMesh}>
+        <Table/>
+        </mesh>
+        </group>
     </>
   );
 };
