a0b42fb28dd345d51eb5beef94e8ee6306affe83
Axel <axelgumiit2@gmail.com>
2024-10-15 16:32:04 +0100

    remove the space bar function

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const Controls = {
   back: "back",
   left: "left",
   right: "right",
-  jump: "jump",
+
 };
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
       { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
       { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
       { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-      { name: Controls.jump, keys: ["Space"] },
+     
     ],
     []
   );
