49dc8c75fa3317f90b2b528be45c2cdbb355b932
Axel <axelgumiit2@gmail.com>
2024-10-16 16:33:39 +0100

    added collision around the plane

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,10 @@
 import { Environment, OrbitControls, Cylinder } from "@react-three/drei";
 import { useRef, useState } from "react";
-import { Physics, RigidBody, CylinderCollider} from "@react-three/rapier";
+import { Physics, RigidBody, CylinderCollider, CuboidCollider } from "@react-three/rapier";
 import { AvatarController } from "./AvatarController.jsx";
 import { WorkDesk } from "./desk.jsx";
-import { Plane } from "@react-three/drei"
-
-
-
-
-
+import { Plane } from "@react-three/drei";
+import { Html } from '@react-three/drei';
 
 export const Experience = () => {
   const myMesh = useRef();
@@ -18,26 +14,32 @@ export const Experience = () => {
   };
   const [hovered, setHovered] = useState(false);
 
-
- 
-
   return (
     <>
-    
       <Environment preset="sunset" />
       <ambientLight intensity={0.5} />
-    
+
       <Physics>
         <group position-y={-1}>
           <RigidBody colliders={false} type="fixed" position-y={-0.5} friction={2}>
             <CylinderCollider args={[1, 10]} />
             
-              <Plane args={[10, 10]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 1, 0]} >
-                <meshStandardMaterial attach="material" color="#DAB1DA" />
-              </Plane>
+            <Plane args={[10, 10]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 1, 0]}>
+              <meshStandardMaterial attach="material" color="#DAB1DA" />
+            </Plane>
 
-           
-            
+            {/* Boundary Colliders */}
+            <RigidBody type="fixed" colliders="hull">
+              {/* Left Boundary */}
+              <CuboidCollider args={[0.5, 10, 10]} position={[-5, 5, 0]} />
+              {/* Right Boundary */}
+              <CuboidCollider args={[0.5, 10, 10]} position={[5, 5, 0]} />
+              {/* Front Boundary */}
+              <CuboidCollider args={[10, 10, 0.5]} position={[0, 5, -5]} />
+              {/* Back Boundary */}
+              <CuboidCollider args={[10, 10, 0.5]} position={[0, 5, 5]} />
+            </RigidBody>
+
             <group
               position={[0, 1, 4]}
               rotation={[0, Math.PI, 0]}
@@ -45,28 +47,35 @@ export const Experience = () => {
               onPointerOver={() => setHovered(true)} 
               onPointerOut={() => setHovered(false)} 
             >
-                <RigidBody type="fixed" colliders="cuboid">
+              <RigidBody type="fixed" colliders="cuboid">
                 <mesh scale={hovered ? 1.2 : 1}>
                   <WorkDesk />
                   <boxGeometry args={[1, 1, 1]} />
                   <meshBasicMaterial transparent opacity={0} />
                 </mesh>
               </RigidBody>
+              {hovered && (
+                <Html position={[0, 2, 0]} center>
+                  <div style={{ 
+                    background: 'white', 
+                    borderRadius: '10px', 
+                    width: '100px',
+                    fontSize: '20px',
+                    padding: '5px', 
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
+                  }}>
+                    Check Out the Project
+                  </div>
+                </Html>
+              )}
             </group>
-
           </RigidBody>
 
-          <AvatarController/>
-          </group>
-        
+          <AvatarController />
+        </group>
       </Physics>
 
       {scene === "deskScene" && <DeskScene />}
-
-     
-
-      
-      
     </>
   );
 };
