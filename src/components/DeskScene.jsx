7bd0650bba66639e23415426bae4e3f26f8fe5e8
Axel <axelgumiit2@gmail.com>
2024-10-16 18:57:26 +0100

    configure the deskscene jsx file

--- src/components/DeskScene.jsx
+++ src/components/DeskScene.jsx
@@ -1,9 +1,40 @@
-const DeskScene = () => {
-    return (
-      <group>
-        
-        <ambientLight intensity={0.5} />
-        
+import { WorkDesk } from "./desk";
+import { Environment, OrbitControls, Html } from '@react-three/drei';
+import { useState } from 'react';
+
+export const DeskScene = () => {
+  const [hovered, setHovered] = useState(false); // State to manage hover effect
+
+  return (
+    <group>
+      <Environment preset="sunset" />
+      <ambientLight intensity={1} />
+      <OrbitControls />
+
+      <group
+        scale={3.5}
+        position={[-1, -4, -1]} // Adjust position as needed
+        rotation={[0, Math.PI / 4, 0]}
+        onPointerOver={() => setHovered(true)} 
+        onPointerOut={() => setHovered(false)} 
+      >
+        <WorkDesk />
       </group>
-    );
-  };
+
+      
+        <Html position={[0, 2, 0]} center>
+          <div style={{ 
+            background: '#DAB1DA', 
+            borderRadius: '10px', 
+            width: '1000px',
+            fontSize: '100px',
+            padding: '5px', 
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)'
+          }}>
+            Welcome to Project page
+          </div>
+        </Html>
+     
+    </group>
+  );
+};
